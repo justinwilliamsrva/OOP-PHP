272ff059e74f1efd52e589bcde296b5a92340d0a
Justin Williams <justinwdev@gmail.com>
2022-02-15 13:18:07 -0500

    added own excpetion class and also created example with static functions

---
+++ 8_Exceptions.php
@@ -0,0 +1,106 @@
+<?php
+
+
+// ********** ********** **********
+// ********** Create own Exceptions  **********
+// ********** ********** **********
+
+class NotAFloatBro extends Exception{
+
+    protected $message = "give me a float please";
+}
+class NotAPositiveFloatBro extends Exception{
+
+    protected $message = "give me a positive float please";
+}
+
+
+
+function add($one, $two)
+{
+
+    if ($one < 0  || $two < 0) {
+
+        throw new NotAPositiveFloatBro;
+    }
+
+    if (!is_float($one) || !is_float($two)) {
+
+        throw new NotAFloatBro;
+    }
+
+    return $one + $two;
+}
+
+
+// try {
+//     echo add(2.0, 2.0);
+//     echo "\n";
+// echo add(2.0, -2);
+  echo add(2.0, []);
+
+
+// } catch (NotAFloatBro $e) {
+
+//    var_dump($e);
+// } catch (NotAPositiveFloatBro $e) {
+
+//     var_dump($e);
+// }
+
+// ********** ********** **********
+// ********** Create own Exceptions with static classes  **********
+// ********** ********** **********
+
+
+class TeamException extends Exception{
+
+    public static function NotAFloatBro(){
+
+        return new static("give me a float please");
+    }
+    public static function NotAPositiveFloatBro(){
+
+        return new static("give me a positive float please");
+    }
+}
+
+
+
+
+
+function adding($one, $two)
+{
+
+    if ($one < 0  || $two < 0) {
+
+        throw TeamException::NotAPositiveFloatBro();
+    }
+
+    if (!is_float($one) || !is_float($two)) {
+
+
+        throw TeamException::NotAFloatBro();
+
+    }
+
+    return $one + $two;
+}
+
+
+try {
+    echo add(2.0, 2.0);
+    echo "\n";
+    // echo add(2.0, -2);
+    echo add(2.0, []);
+
+
+
+
+} catch (NotAFloatBro $e) {
+
+   var_dump($e);
+} catch (NotAPositiveFloatBro $e) {
+
+    var_dump($e);
+}
